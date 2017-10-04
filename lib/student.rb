594ddc472cae87c2d616aa9b282f6204c1a1595a
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 17:48:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,14 @@ class Student
     new_student
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL 
+    SELECT * FROM students 
+    WHERE name = ? LIMIT 1
+    SQL 
+    DB[:conn].execute(sql, name).map do |row|
+    
+
 
 
 
